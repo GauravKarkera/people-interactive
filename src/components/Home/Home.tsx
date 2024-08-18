ab30a1602220ed567a31a393ba5ac7dd5eda8d9d
GauravKarkera <gauravkarkera18@gmail.com>
2024-08-18 20:49:37 +0530

    test change

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -25,6 +25,8 @@ export default function Home(props: IHomeProps) {
         if (node) observer.current.observe(node)
     }, [loading, observer, contactListError]);
 
+    console.log("Test");
+     
     function fetchContacts() {
         setLoading(true)
         setContactListError(false)
